530d9105b16d54e1f9cae56b5ded6ec44c244326
ViniMeloSoares <vinimelosoares@gmail.com>
2025-02-10 00:37:30 -0300

    ViewBinding RadioGrup RadioButtons Buttons

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.tipcalculator
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tipcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+ private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.brOptionOne.setOnCheckedChangeListener { _, isChecked -> }
+        binding.brOptionTwo.setOnCheckedChangeListener { _, isChecked -> }
+        binding.brOpitionThree.setOnCheckedChangeListener { _, isChecked -> }
+
+       binding.btnClean.setOnClickListener {  }
+
+
+
     }
 }
