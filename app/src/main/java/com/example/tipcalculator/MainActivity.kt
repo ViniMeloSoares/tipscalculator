2727c9ed797cdf117ad87672f6c37f1cab27cd9b
ViniMeloSoares <vinimelosoares@gmail.com>
2025-02-10 22:14:05 -0300

    Calculo

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -18,13 +18,47 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.brOptionOne.setOnCheckedChangeListener { _, isChecked -> }
-        binding.brOptionTwo.setOnCheckedChangeListener { _, isChecked -> }
-        binding.brOpitionThree.setOnCheckedChangeListener { _, isChecked -> }
+        binding.edtPercentual.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+               binding.rgPercentage.clearCheck()
+            }
+        }
+
+        binding.btnCalculator.setOnClickListener {
+            val totalTable: Float = binding.edtTieTotal.text.toString().toFloat()
+            val NPeople: Int = binding.edtNumberOfPeople.text.toString().toInt()
+
+            var percentage = 0
+            if (binding.edtPercentual.text.toString().isNotEmpty()){
+                percentage = binding.edtPercentual.text.toString().toInt()
+            } else{
+
+        binding.brOptionOne.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                percentage = 10
+            }
+        }
+        binding.brOptionTwo.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                percentage = 15
+            }
+        }
+        binding.brOpitionThree.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                percentage = 20
+            }
+          }
+        }
+            val TotalTemp = totalTable/NPeople
+            val tips = TotalTemp * percentage /100
+            val totalWithTips = TotalTemp + tips
+
+            println(totalWithTips)
+        }
+
 
        binding.btnClean.setOnClickListener {  }
 
